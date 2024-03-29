1886ef0e04ea56330ce261a63588ad8b286a5213
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-29 00:01:03 -0300

    feat: if num > 7, labl will loose fontsize

--- calculator/MainPage.xaml.cs
+++ calculator/MainPage.xaml.cs
@@ -14,12 +14,26 @@ namespace calculator
         {
             InitializeComponent();
         }
-        public void AddNumber(Object sender, EventArgs e)
+        void AddNumber(Object sender, EventArgs e)
         {
+            if (result.Text == "0") {
+                result.Text = "";
+            }
+
             Button BtnDown = (Button)sender;
 
             String BtnText = BtnDown.Text;
+            if (result.Text.Length > 7)
+            {
+                result.FontSize -= 10;
+            }
             result.Text += BtnText;
+            
+        }
+        void ClearNumber(Object sender, EventArgs e)
+        {
+            result.Text = "0";
+            result.FontSize = 56;
         }
     }
 }
