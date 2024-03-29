4b5ae7df8740154178ecc3a67d5a91da1038bd07
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-29 19:56:56 -0300

    feat: calculus function

--- calculator/MainPage.xaml.cs
+++ calculator/MainPage.xaml.cs
@@ -10,15 +10,23 @@ namespace calculator
 {
     public partial class MainPage : ContentPage
     {
+        private Boolean plusMinus = false;
+
+        private String mathMethod = "";
+        private String textResult = "";
+
+        private Double? firstNum = null;
+        private Double? secondNum = null;
+
+
         public MainPage()
         {
             InitializeComponent();
         }
         void AddNumber(Object sender, EventArgs e)
         {
-            if (result.Text == "0") {
-                result.Text = "";
-            }
+            result.Text = "";
+            
 
             Button BtnDown = (Button)sender;
 
@@ -30,14 +38,99 @@ namespace calculator
 
             if (result.Text.Length < 9)
             {
-                result.Text += BtnText;
+                this.textResult += BtnText;
             }
+
+            result.Text = this.textResult;
             
         }
         void ClearNumber(Object sender, EventArgs e)
         {
             result.Text = "0";
             result.FontSize = 56;
+
+            this.mathMethod = null;
+            this.textResult = null;
+        }
+        void PlusMinus(Object sender, EventArgs e)
+        {
+            if (this.plusMinus)
+            {
+                this.textResult = this.textResult.Substring(1);
+                this.plusMinus = false;
+            }else
+            {
+                this.textResult = "-" + this.textResult;
+                this.plusMinus = true;
+            }
+
+            result.Text = this.textResult;
+        }
+        void SetMathMethod(Object sender, EventArgs e)
+        {
+            Button BtnDown = (Button)sender;
+            String BtnText = BtnDown.Text;
+
+            try
+            {
+                this.firstNum = double.Parse(this.textResult);
+                this.mathMethod = BtnText;
+
+                this.textResult = null;
+            } catch (Exception)
+            {
+                DisplayAlert("Erro", "Não foi possivel concluir esta ação", "OK");
+            }
+        }
+        void MakeCalculation(Object sender, EventArgs e)
+        {
+            try
+            {
+                this.secondNum = double.Parse(result.Text);
+
+                switch (this.mathMethod)
+                {
+                    case "+":
+                        result.Text = (this.firstNum + this.secondNum).ToString();
+                        break; 
+                    case "−":
+                        result.Text = (this.firstNum - this.secondNum).ToString();
+                        break;
+                    case "×":
+                        result.Text = (this.firstNum * this.secondNum).ToString();
+                        break;
+                    case "÷":
+                        if (this.firstNum != 0 || this.secondNum != 0)
+                        {
+                            result.Text = (this.firstNum / this.secondNum).ToString();
+                        }else
+                        {
+                            result.Text = "Indefinido";
+                        }
+                        break;
+                }
+
+                this.firstNum = 0;
+                this.secondNum = 0;
+            } catch (Exception)
+            {
+                DisplayAlert("Erro", "Não foi possivel concluir esta ação", "OK");
+            }
+        }
+        void Percent(Object sender, EventArgs e)
+        {
+            if (firstNum != null)
+            {
+                this.secondNum = this.secondNum / 100;
+
+                result.Text = this.secondNum.ToString();
+            }
+            else
+            {
+                this.firstNum = this.firstNum / 100;
+
+                result.Text = this.firstNum.ToString();
+            }
         }
     }
 }
